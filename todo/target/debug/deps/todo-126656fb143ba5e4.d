7fd9fed24e59d1d66d03564b998e7ed1de33c392
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-02-05 23:09:21 +0900

    5min

--- todo/target/debug/deps/todo-126656fb143ba5e4.d
+++
@@ -1,5 +0,0 @@
-/home/kiririn/git/rust_CLI/todo/target/debug/deps/todo-126656fb143ba5e4.rmeta: src/main.rs
-
-/home/kiririn/git/rust_CLI/todo/target/debug/deps/todo-126656fb143ba5e4.d: src/main.rs
-
-src/main.rs:
--- todo/target/debug/todo.d
+++
@@ -1,1 +0,0 @@
-/home/kiririn/git/rust_CLI/todo/target/debug/todo: /home/kiririn/git/rust_CLI/todo/src/main.rs
