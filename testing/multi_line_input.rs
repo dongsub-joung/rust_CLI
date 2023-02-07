39f05b91b8caf22896fe0d9d4f2ab195ae30ae54
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-02-08 03:38:25 +0900

    match type

---
+++ testing/multi_line_input.rs
@@ -0,0 +1,24 @@
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()>{
+ let mut lines= io::stdin().lock().lines();
+ let mut user_input= String::new();
+
+ while let Some(line)= lines.next() {
+     let last_input= line.unwrap();
+     
+     if last_input.len() == 0 {
+         break;
+     }
+
+     if user_input.len() > 0 {
+         user_input.push_str("\n");
+     }
+
+     user_input.push_str(&last_input);
+     }
+
+     println!("\n input \n {}", user_input);
+
+     Ok(())
+}
