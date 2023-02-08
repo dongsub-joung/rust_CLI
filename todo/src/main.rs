98c72f14bc2e707164db823728a585899b07db4c
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-02-08 22:12:17 +0900

    progress

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -7,17 +7,28 @@ fn main() {
         let mut list: Vec<String>= Vec::new();
     
         println!("Press a button\n");
-        println!("1. Show up list\n");
-        println!("2. Add a todo\n");
-        println!("3. Delete a todo\n");
+        println!("1. Add a todo\n");
+        println!("2. Delete a todo\n");
        
         let number= inputing();
        
+        
+
         match number{
             0 => break,
-            1 => list_up(list),
-            2 => list= adding(list),
-            3 => list= deleting(list),
+            1 => {
+                let list_backup= list.clone();
+                let added_list= adding(list_backup);
+                list= added_list.clone();
+                list_up(added_list);
+                
+            },
+            2 => {
+                let list_backup= list.clone();
+                let deleted_list= deleting(list_backup);
+                list= deleted_list.clone();
+                list_up(deleted_list);
+            },
             _ => list_up(list),
         }
     }
