f01d2ed1d7e1738cfffa43242ed603c400b724b2
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-02-19 16:54:03 +0900

    undo

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,8 +1,7 @@
-mod crud;
-
 use std::io::{self, stdin};
-use crud::Crud::*;
 
+// @todo
+// Need deep copy
 fn main() {
     loop{
         let mut list: Vec<String>= Vec::new();
@@ -12,7 +11,7 @@ fn main() {
         println!("2. Delete a todo\n");
        
         let number= inputing();
-
+       
         match number{
             0 => break,
             1 => {
@@ -31,4 +30,45 @@ fn main() {
             _ => list_up(list),
         }
     }
+
+    fn list_up(list: Vec<String>){
+        println!("{:?}", list)
+    }
+
+    fn adding(list: Vec<String>) -> Vec<String>{
+        let inputed= inputing_string();
+        let mut list_new= list.clone();
+
+        list_new.push(inputed);
+
+        list_new
+    }
+
+    fn deleting(list: Vec<String>) -> Vec<String>{
+        let index= inputing();
+        let mut list_new= list.clone();
+
+        list_new.remove(index as usize);
+
+        list_new
+    }
+
+    fn inputing() -> i32{ 
+        let mut std_number= String::new();   
+        stdin().read_line(&mut std_number).unwrap();
+        let result :usize= std_number.trim_end().parse().unwrap();
+        
+        result as i32 
+    }
+
+    fn inputing_string() -> String{
+        let mut inputed= String::new();
+        stdin().read_line(&mut inputed).unwrap();
+        
+        let result= inputed.trim_end().to_string();
+
+        result
+    }
 }
+
+
