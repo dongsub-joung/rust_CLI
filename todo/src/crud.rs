0f905235e2c3d227a38c6072dd04b03278935b90
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-12 22:32:27 +0900

    refactoring

---
+++ todo/src/crud.rs
@@ -0,0 +1,16 @@
+pub mod crud{
+    pub fn add(){         
+        println!("add");
+    }
+    pub fn list_up(){
+        println!("list up"); 
+    }
+
+    pub fn update(){
+        println!("update");
+    }
+
+    pub fn delete(){
+        println!("del");
+    }
+}
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,56 +1,75 @@
-mod tools;
+mod crud;
 
-use tools;
-use std::io::{self, stdin};
+use std::io::{self, stdin, Error};
+
+fn intro() {
+    println!("  _   _      _ _        __        __         _     _ _ _");
+    println!(" | | | | ___| | | ___   \\ \\      / /__  _ __| | __| | | |");
+    println!(" | |_| |/ _ \\ | |/ _ \\   \\ \\ /\\ / / _ \\| '__| |/ _` | | |");
+    println!(" |  _  |  __/ | | (_) |   \\ V  V / (_) | |  | | (_| |_|_|");
+    println!(" |_| |_|\\___|_|_|\\___/     \\_/\\_/ \\___/|_|  |_|\\__,_(_|_)");
+}
+
+fn inputing() -> Result<i32, Error>{ 
+    let mut std_number= String::new();   
+    stdin().read_line(&mut std_number).unwrap();
+    let result :usize= std_number.trim_end().parse().unwrap();
+    
+    Ok(result as i32) 
+}
+
+fn inputing_string() -> Result<String, Error>{
+    let mut inputed= String::new();
+    stdin().read_line(&mut inputed).unwrap();
+    
+    let result= inputed.trim_end().to_string();
+
+    Ok(result)
+}
+
+fn menu(){
+    println!("\n");
+    println!("\n");
+    println!("Press a button\n");
+    println!("1. Add a todo\n");
+    println!("2. Update a todo\n");
+    println!("3. Delete a todo\n");
+}
 
 fn main() {
+    const IO_ERR:& str= "invalied input";
+    const NOT_INT: &str= "invalied input: not int";
+
     loop{
         let mut list: Vec<String>= Vec::new();
-    
-        println!("Press a button\n");
-        println!("1. Add a todo\n");
-        println!("2. Delete a todo\n");
+        
+        intro();
+        menu();
        
-        let number= inputing();
+        let number= inputing().expect(NOT_INT);
        
         match number{
             0 => break,
             1 => {
-                let inputed= inputing_string();
+                let inputed= inputing_string().expect(IO_ERR);
                 
-                let list= list.clone();
-                
-                let added_list= tools::Add::adding(inputed, list);
-                
-                tools::List::list_up(&added_list);
+                crud::crud::add();
+                crud::crud::list_up();
             },
             2 => {
-                let index= inputing();
+                let index= inputing().expect(NOT_INT);
                 
-                let deleted_list= tools::Delete::deleting(index, list_backup);
-                
-                tools::List::list_up(&deleted_list);
+                crud::crud::update();
+                crud::crud::list_up();
             },
-            _ => tools::List::list_up(&list),
+            3 => {
+                let index= inputing().expect(NOT_INT);
+                
+                crud::crud::delete();
+                crud::crud::list_up();
+            },
+            _ => crud::List::list_up(),
         }
-    }
-
-
-    fn inputing() -> i32{ 
-        let mut std_number= String::new();   
-        stdin().read_line(&mut std_number).unwrap();
-        let result :usize= std_number.trim_end().parse().unwrap();
-        
-        result as i32 
-    }
-
-    fn inputing_string() -> String{
-        let mut inputed= String::new();
-        stdin().read_line(&mut inputed).unwrap();
-        
-        let result= inputed.trim_end().to_string();
-
-        result
     }
 }
 
